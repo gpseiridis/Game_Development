dc3def1783ba556515a8e35252544403c70a102a
GomUser1 <g.pseiridis@gmail.com>
2016-10-08 01:57:43 +0200

    added bricks, ability to destroy them, normalized the speed of ball after collision

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float ballForce;
+    public float ballSpeed;
+    bool gameStarted = false;
+
+    // Use this for initialization
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = rb.velocity.normalized * ballSpeed;
+        if (Input.GetKeyUp(KeyCode.Space) && gameStarted == false){
+            
+            transform.SetParent(null);
+            rb.isKinematic = false;
+            rb.AddForce(new Vector2(ballForce, ballForce),0);
+            gameStarted = true;
+
+        }
+	
+	}
+}
---
+++ Assets/Scripts/BrickScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D col)
+
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+           // Destroy(gameObject);
+           // added this to make it bounce back. Left it commented in case I change my mind later
+            Destroy(gameObject, 0.2f); 
+        }
+    }
+}
