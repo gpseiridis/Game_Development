5f80af4c50e1aba0e56b9c6e0228fdc4ee9b890e
GomUser1 <g.pseiridis@gmail.com>
2016-10-08 18:52:03 +0200

    removed dead code, normalized the starting point

--- Assets/Scripts/PaddleScript.cs
+++ Assets/Scripts/PaddleScript.cs
@@ -1,72 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PaddleScript : MonoBehaviour {
+public class PaddleScript : MonoBehaviour
+{
 
     public Rigidbody2D rb;
     public float speed;
     public float maxX;
-    public bool screenTouch =false;
+    public bool screenTouch = false;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
-        float y_pos = 95.5f;
+        float y_pos = 120f;
 
-       
-        if(Input.GetMouseButtonDown(0))
+      
+        print("transform pos = " + transform.position);
+        if (Input.GetMouseButtonDown(0))
         {
             screenTouch = true;
-           
-        }
-        if (screenTouch != false) { 
-       
-            Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, y_pos);
-            Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-            transform.position = curPosition;
 
         }
-
-        float x = Input.GetAxis("Horizontal");
-
-        //movement
-        if (x == 0)
+        if (screenTouch != false)
         {
-            Stop();
+         
+            Vector2 currScreenPoint = new Vector2(Input.mousePosition.x, y_pos);
+            Vector2 currPosition = Camera.main.ScreenToWorldPoint(currScreenPoint);
+            transform.position = currPosition;
+
         }
-        else if (x > 0)
-        {
-            MoveRight();
-        }
-        else if(x <0)
-        {
-            MoveLeft();
-        }
-        //restrict the movement outside boundaries
-        Vector3 pos = transform.position;
-        pos.x= Mathf.Clamp(pos.x, -maxX, maxX);
-        transform.position = pos;
+
+
+
 
     }
-
-    void MoveLeft()
-    {
-        rb.velocity = new Vector2(-speed, 0);
-    }
-    void MoveRight()
-    {
-        rb.velocity = new Vector2(speed, 0);
-    }
-
-    void Stop()
-    {
-        rb.velocity = Vector2.zero;
-    }
-   
-    
 }
