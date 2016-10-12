fd24794ff542651dde1ddf2cabdc2dc1af5a7802
GomUser1 <g.pseiridis@gmail.com>
2016-10-13 01:02:31 +0200

    added the ability to destroy the bricks based on maximum hits. Also the sprite changes to look more broken

--- Assets/Scripts/BrickScript.cs
+++ Assets/Scripts/BrickScript.cs
@@ -5,11 +5,15 @@ public class BrickScript : MonoBehaviour {
 
     public int maxHits;
     private int timesHit;
+    private LevelManager levelManager;
+    public Sprite[] spritesArray;
+    public static int bricks  = 0;
 
     // Use this for initialization
     void Start() {
 
         timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
 
     }
 
@@ -21,7 +25,28 @@ public class BrickScript : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D coll)
     {
         //count how many times got hit
-        timesHit++; 
+        timesHit++;
+        if(timesHit == maxHits) { 
+        Destroy(gameObject);
+        }
+        else
+        {
+            nextSprite();
+        }
+       
 
+    }
+
+    void nextSprite()
+    {
+        //if hit 1 time then we get sprite 1-1=0 index
+        int spriteIndex = timesHit - 1;
+        this.GetComponent<SpriteRenderer>().sprite = spritesArray[spriteIndex];
+    }
+    //TDOO remove when we actually win
+    void YouWin()
+    {
+
+        levelManager.LoadNextLevel();
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,4 +13,12 @@ public class LevelManager : MonoBehaviour {
 		Application.Quit ();
 	}
 
+    public void LoadNextLevel()
+    {
+        //pass level based on the build settings and index.
+        // level 1 ---> level 2 ---> .... ---> level N ---> Win Screen
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
 }
+ 
